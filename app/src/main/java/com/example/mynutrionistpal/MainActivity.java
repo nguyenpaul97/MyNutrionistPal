6ef485cb7d99c7699e1d0c380f4040d33c1233da
nguyenpaul97 <nguyenpaul.ic.1997@gmail.com>
2020-02-06 11:16:45 -0800

    Wrote code that reads in JSON file

--- app/src/main/java/com/example/mynutrionistpal/MainActivity.java
+++ app/src/main/java/com/example/mynutrionistpal/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mynutrionistpal;
 
+import android.app.ActivityManager;
 import android.os.Bundle;
-
+import android.os.Environment;
+import android.content.Context;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,22 +11,42 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.navigation.fragment.NavHostFragment;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.InputStream;
+import java.io.IOException;
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+       super.onCreate(savedInstanceState);
+       setContentView((R.layout.activity_main));
+    }
+    public void loadJSONFromAsset() {
+        String json = null;
+        try {
+            InputStream is = getAssets().open("Food_Display_Table_json.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        try {
+            JSONObject obj = new JSONObject();
+            JSONArray m_jArry = obj.getJSONArray(json);
+            System.out.println((m_jArry));
+
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
